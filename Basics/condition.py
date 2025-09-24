c74b766f5683f5af08d08caed3d16290faebe4e0
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-09-24 21:49:01 +0530

    learning

---
+++ Basics/condition.py
@@ -0,0 +1,8 @@
+temperature = int(input("Temperature: "))
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("It's nom day")
+else : 
+    print("It's a cold day")
---
+++ Basics/input.py
@@ -0,0 +1,9 @@
+x = input("x: ")
+y= int(x) + 125
+print(f"x + 1 = {y}")  
+
+z = int(x)
+if z > y:
+     print("z is greater than y")
+else:
+     print("z is less than ")  
