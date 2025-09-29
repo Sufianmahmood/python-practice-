d275322b87f32402775676f6cae8e6ebb0b64a05
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-09-29 13:00:33 +0530

    fuctins

---
+++ Basics/function.py
@@ -0,0 +1,5 @@
+def greet (): 
+    Firstname = input ("Enter your First name: ")   
+    Lastname = input ("Enter your Last name: ")
+    print (f"Welcome {Firstname} {Lastname} ") 
+greet ()
---
+++ Basics/task2.py
@@ -0,0 +1,4 @@
+number = 2000
+while number > 0:
+    print (number)
+    number = number // 2
